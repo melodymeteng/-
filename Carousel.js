6eb84a5ab307151e8ae7f03d86605e228b4e233b
melodymateng <370627276@qq.com>
2017-12-23 16:19:29 +0800

    封装轮播图的基本样式与JS部分 阿西吧 母鸡是哪里的问题准备回去解决

--- Carousel.js
+++ Carousel.js
@@ -1,7 +1,7 @@
 /**
  * Created by apple on 17/12/23.
  */
-define(function(){
+define(['jquery'],function($){
     function Carousel(){
 
         this.$container = $('<div class="carousel-container"></div>');
@@ -9,7 +9,58 @@ define(function(){
         this.$tab = $('<ul class="carousel-tab"></ul>');
         this.$prev = $('<span class="carousel-prev">&lt;</span>');
         this.$next = $('<span class="carousel-next">&gt;</span>');
+        this.defaultOption={
+            buttonType:"circle",
+            btnPos:"center",
+            speed:"1000"
+        }
     }
+    Carousel.prototype.init=function (option) {
+        $.extend(this.defaultOption,option);
+        var _this=this;
+        this.$container.append(this.$imgBox).append( this.$tab).append(this.$prev).append(this.$next );
+        for(var i=0;i<this.defaultOption.imgDate.length;i++) {
+            console.log('a');
+                var cls = (i == 0) ? "selected" : "";
+            this.$imgBox.append($('<img class="' + cls + '" src="' + this.defaultOption.imgDate[i] + '"/>'));
+            this.$tab.append($('<li class="' + this.defaultOption.buttonType + cls + '">' + (i + 1) + '</li>'));
+        }
+
+       this.$prev.addClass("carousel-prev-"+this.defaultOption.btnPos);
+        this.$next.addClass("carousel-next-"+this.defaultOption.btnPos);
+        $(this.defaultOption.selector).append(this.$container);
+        $('li',this.$tab).on('click',function () {
+            changeImg($(this).index());
+            iNow=$(this).index();
+        });
+        function changeImg(idx) {
+            $('li',this.$tab).eq(idx).addClass('selected').siblings().removeClass('selected');
+            $('img',this.$imgBox).eq(idx).addClass('selected').siblings().removeClass('selected');
+        }
+        this.$prev.on('click',function () {
+            iNow--;
+            if(iNow==-1){
+                iNow=_this.defaultOption.imgDate.length-1;
+            }
+            changeImg(iNow);
+        });
+        this.$next.on('click',function () {
+            iNow++;
+            if(iNow==-1){
+                iNow=_this.defaultOption.imgDate.length-1;
+            }
+            changeImg(iNow);
+        });
+
+
+
+        $(this.defaultOption.selector).append(this.$container);
+
+
+
+    }
+
+
 
 
 
--- index.js
+++ index.js
@@ -2,6 +2,21 @@
  * Created by apple on 17/12/23.
  */
 require(['Carousel'],function(Carousel){
-
+    var carousel1=new Carousel();
+    carousel1.init({
+        selector:"#content1",
+        imgDate:['img/1.jpg','img/2.jpg','img/3.jpg','img/4.jpg'],
+        buttonType:"squire",
+        btnPos:"bottom",
+        speed:"2000"
+    });
+    var carousel2 = new Carousel();
+    carousel2.init({
+        selector:"#content2",
+        imgDate:['img/1.jpg','img/2.jpg','img/3.jpg','img/4.jpg'],
+        buttonType:"circle",
+        btnPos:"center",
+        speed:"1000"
+    });
 
 });
